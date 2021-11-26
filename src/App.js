e653794ce77567aeea7490720d5c335d2f26d7dc
wahidunutas <wahidariesta@gmail.com>
2021-11-26 22:03:40 +0700

    react-navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from './Components/Navbar/Navbar';
 import './App.css';
+import Home from './Components/pages/Home';
+import Services from './Components/pages/Services';
+import Products from './Components/pages/Products';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/services' component={Services} />
+        <Route path='/products' component={Products} />
+      </Switch>
+    </Router>
   );
 }
 
